edb6f7209050f2efec357671fa0be62e92a36cdb
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2023-01-08 23:24:45 +0100

    update script.js

--- with permission (Api)/script.js
+++ with permission (Api)/script.js
@@ -1,3 +1,12 @@
 const getlocation = () => { 
     // get the location form the user
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;
+            console.log(lat,long);
+        });
+    } else{
+        alert("geolocation is not allowed by this browser");
+    }
 }
